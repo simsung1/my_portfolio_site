24ac1540cc5685a450adbb475ca8ba483aa8343d
simsung1 <sungmin.sim@outlook.com>
2024-09-29 17:06:04 -0400

    refactor: change the styling of chevron button in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,12 @@ import { ChevronDownIcon } from '@heroicons/react/16/solid';
 
 function Hero() {
   return (
-    <section className="flex items-center justify-center h-screen bg-black bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${spaceHalo})` }}>
+    <section className="flex flex-col items-center justify-center h-screen bg-black bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${spaceHalo})` }}>
       <div className="text-center text-white p-6">
         <h1 className="text-5xl font-medium mb-4 font-fredoka">Hello, I'm Sung</h1>
         <p className="text-md mb-6">Student Web Developer and Mobile Applications Enthusiast</p>
-        <a href='#about'><ChevronDownIcon className='size-10 text-gray-400 hover:text-white'/></a>
       </div>
+      <a href='#about'><ChevronDownIcon className='size-10 text-gray-400 hover:text-white'/></a>
     </section>
   );
 }
